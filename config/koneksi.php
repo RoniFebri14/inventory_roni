f272d07a3f8684d6d2e8c93876cce47babeedbb6
RoniFebri14 <ronifebrimarhas@gmail.com>
2025-04-10 13:15:28 +0700

    push terakhir

---
+++ config/koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+$hostname= 'localhost'; 
+$username= 'root'; 
+$password=''; 
+$database_name ='db_inventory_roni'; 
+
+$conn = mysqli_connect($hostname, $username, $password, $database_name); 
+
+if(!$conn){ 
+die("Koneksi Gagal : " . mysqli_connect_error()); 
+}
---
+++ database/db_inventory_roni.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 10, 2025 at 02:57 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_inventory_roni`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int NOT NULL,
+  `nama_barang` varchar(20) DEFAULT NULL,
+  `id_jenis` varchar(20) DEFAULT NULL,
+  `harga` varchar(20) DEFAULT NULL,
+  `stock` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `id_jenis`, `harga`, `stock`) VALUES
+(1, 'baju', '101', '500000', '40'),
+(2, 'celana', '102', '750000', '35'),
+(3, 'tas', '103', '800000', '17'),
+(4, 'sepatu', '104', '600000', '25'),
+(5, 'cincin', '105', '4500000', '13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis`
+--
+
+CREATE TABLE `jenis` (
+  `id_jenis` int NOT NULL,
+  `nama_jenis` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `jenis`
+--
+
+INSERT INTO `jenis` (`id_jenis`, `nama_jenis`) VALUES
+(101, 'pollo'),
+(102, 'cargo'),
+(103, 'ransel'),
+(104, 'vans'),
+(105, 'emas');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `jenis`
+--
+ALTER TABLE `jenis`
+  ADD PRIMARY KEY (`id_jenis`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
